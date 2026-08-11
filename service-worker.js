27ab822ad7aed093fdfc2f27596603324c688b88
Adrien_Rbr <y9zvc4v6wf@privaterelay.appleid.com>
2026-08-11 15:07:35 +0200

    Update cache name to planning-equipe-v7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'planning-equipe-v6';
+const CACHE_NAME = 'planning-equipe-v7';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/apple-touch-icon.png', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
